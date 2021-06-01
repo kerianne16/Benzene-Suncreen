8978c9fa0cc5756cd33d32ff20076d54f2988a82
Keri Levesque <kerianne166@gmail.com>
2021-06-01 13:55:41 -0400

    added base project

---
+++ BenzeneSunscreen/BenzeneSunscreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BenzeneSunscreen
+//
+//  Created by Keri Levesque on 6/1/21.
+//  Copyright © 2021 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
