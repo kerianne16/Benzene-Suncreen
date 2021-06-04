daed63ac222535aec438ef886e7ff402f4f99313
Keri Levesque <kerianne166@gmail.com>
2021-06-04 11:21:34 -0400

    model file, starting main storyboard and adding more json

---
+++ BenzeneSunscreen/BenzeneSunscreen/Model/Sunscreen.swift
@@ -0,0 +1,24 @@
+//
+//  Sunscreen.swift
+//  BenzeneSunscreen
+//
+//  Created by Keri Levesque on 6/4/21.
+//  Copyright © 2021 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+
+struct Sunscreen: Codable {
+    
+    let brandName: String
+    let type: String
+    let description: String
+    let spf: String
+    let upc: String
+    let lot: String
+    let expiration: String
+    let activePharmacuticalIngredients: String
+    let benzeneAvgPpm: String
+    let percent: String
+}
