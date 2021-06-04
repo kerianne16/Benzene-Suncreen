0dc3aa949481241950b62b0e828dfaee6719ee03
Keri Levesque <kerianne166@gmail.com>
2021-06-04 11:30:42 -0400

    table view cell / controller files

---
+++ BenzeneSunscreen/BenzeneSunscreen/Views/MainSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainSearchTableViewCell.swift
+//  BenzeneSunscreen
+//
+//  Created by Keri Levesque on 6/4/21.
+//  Copyright © 2021 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class MainSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
